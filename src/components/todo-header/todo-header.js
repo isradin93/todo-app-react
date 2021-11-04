6d0d3576c8588612ebfeb050b557e7afb0a97e35
Isradin <isradin.nurbek@gmail.com>
2021-11-04 21:21:36 +0300

    Add todo icon

--- src/components/todo-header/todo-header.js
+++ src/components/todo-header/todo-header.js
@@ -3,6 +3,10 @@ import './todo-header.css';
 const TodoHeader = () => {
     return (
         <header className="header">
+            <img src="/images/todo-icon.png"
+                className="todo-img"
+                alt="todo-icon"
+            />
             <h1>Todo List</h1>
         </header>
     );
